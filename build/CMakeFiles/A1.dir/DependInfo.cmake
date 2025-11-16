2588901a189c36c6a143e20a00836efbed944ddc
nikkol615 <nik.kolobanov@bk.ru>
2025-11-17 01:21:14 +0300

    update: docs

---
+++ build/CMakeFiles/A1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nikko/Documents/hse_cpp/set3/A1/Circle.cpp" "CMakeFiles/A1.dir/Circle.cpp.o" "gcc" "CMakeFiles/A1.dir/Circle.cpp.o.d"
+  "/Users/nikko/Documents/hse_cpp/set3/A1/intersArea.cpp" "CMakeFiles/A1.dir/intersArea.cpp.o" "gcc" "CMakeFiles/A1.dir/intersArea.cpp.o.d"
+  "/Users/nikko/Documents/hse_cpp/set3/A1/main.cpp" "CMakeFiles/A1.dir/main.cpp.o" "gcc" "CMakeFiles/A1.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/A1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "A1"
+  "A1.pdb"
+  "CMakeFiles/A1.dir/Circle.cpp.o"
+  "CMakeFiles/A1.dir/Circle.cpp.o.d"
+  "CMakeFiles/A1.dir/intersArea.cpp.o"
+  "CMakeFiles/A1.dir/intersArea.cpp.o.d"
+  "CMakeFiles/A1.dir/main.cpp.o"
+  "CMakeFiles/A1.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/A1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
